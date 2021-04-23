f5805810f45d7f19da45db34c84dcb7c0a8d4da4
Amro dandashli <amrdandashli@gmail.com>
2021-04-23 01:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
 
-    return fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => renderBooks(json));
-  }
-
-
+    return response.json();
+  })
+  .then(function(json) {
+renderBooks.json()
+  });
+  return fetch()
+}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
